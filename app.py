d9f740dd2781573272dedbff7b2fa116400e9d3c
triph0711 <126488937+triph0711@users.noreply.github.com>
2024-12-21 02:29:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,2 @@
 import streamlit as st
-#import openai
-import speech_recognition as sr
-
-def transcribe_speech():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        st.write("Speak now...")
-        audio = r.listen(source)
-
-    try:
-        text = r.recognize_google(audio)
-        st.write("You said:", text)
-
-    except sr.UnknownValueError:
-        st.write("Sorry, I could not understand your speech.")
-    except sr.RequestError as e:
-        st.write(f"Could not request results from Google Speech Recognition service; {e}")
-
-# Streamlit app
-st.title("Speech-to-Text App")
-
-if st.button("Start Recording"):
-    transcribe_speech()
+st.write("helllo")
